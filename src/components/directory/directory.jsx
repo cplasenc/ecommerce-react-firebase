45cbc0c4f036924ab048ffecace0575dbfdecaf1
Lebrac <36932780+ctnplasencia@users.noreply.github.com>
2020-07-05 02:09:05 +0100

    rutas con react-router

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MenuItem from '../menu-item/menu-item.component';
-import './directory.styles.scss';
+import MenuItem from '../menu-item/menu-item';
+import './directory.scss';
 
 class Directory extends React.Component {
     constructor() {
@@ -11,26 +11,31 @@ class Directory extends React.Component {
                 {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    linkUrl: 'hats',
                     id: 1
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    linkUrl: 'jackets',
                     id: 2
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    linkUrl: 'sneakers',
                     id: 3
                 },
                 {
                     title: 'womens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    linkUrl: 'womens',
                     id: 4
                 },
                 {
                     title: 'mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    linkUrl: 'mens',
                     id: 5
                 }
             ]
@@ -41,8 +46,12 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 { 
-                this.state.sections.map(({ title, imageUrl, id}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} />
+                this.state.sections.map(({ id, title, imageUrl, linkUrl}) => (
+                    <MenuItem 
+                        key={id} 
+                        title={title} 
+                        imageUrl={imageUrl}
+                        linkUrl={linkUrl} />
                 )) 
                 }
             </div>
