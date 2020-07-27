a2ceaba53d008c2a37d134fb24f35b9e201492c6
C. Plasencia <36932780+ctnplasencia@users.noreply.github.com>
2020-07-27 22:26:18 +0100

    correcion de errores

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -67,7 +67,8 @@ export const convertCollectionsSnapshotToMap = (collections) => {
       items
     }
   });
-  transformedCollection.reduce((accumulator, collection) => {
+
+  return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
   }, {});
